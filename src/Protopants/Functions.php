f4f102ddbf9aad4fe833db7e6308c10931c4fbf3
Julien Desrosiers <juliend2@gmail.com>
2025-05-21 21:14:37 -0400

    Document the functions

--- src/Protopants/Functions.php
+++ src/Protopants/Functions.php
@@ -5,11 +5,18 @@ namespace Protopants;
 use Protopants\Prototype;
 use Protopants\SubPrototype;
 
+/**
+ * Given a prototype, it will allow you to add or override any method.
+ */
 function setMethod(string $prototypeName, string $methodName, callable $methodBody) {
   $prototype = Prototype::get($prototypeName);
   $prototype->setSingleMethod([$methodName => $methodBody]);
 }
 
+/**
+ * Creates an object of a given prototype, with optional params to initialize
+ * the object (overrides the default object's params).
+ */
 function create(string $prototypeName, array $initialParams = []) {
   $parentPrototype = Prototype::get($prototypeName);
   $cloned = new SubPrototype($initialParams ?? [], []);
@@ -18,6 +25,14 @@ function create(string $prototypeName, array $initialParams = []) {
   return $cloned;
 }
 
+/**
+ * Given an array like this example:
+ *  [1, 2, 3, 4]
+ * It will loop such that the iterations will look like the following:
+ *  [3, 4, false]
+ *  [2, 3, false]
+ *  [1, 2, true] <-- this is the last, so the 3rd value is true.
+ */
 function __slidingPairs(array $arr): \Generator {
     for ($i = 0; $i < count($arr) - 1; $i++) {
         yield [$arr[$i], $arr[$i + 1], array_key_last($arr) == $i + 1];
